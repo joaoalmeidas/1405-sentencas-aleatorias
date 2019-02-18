98873f3d01d91f8457a5d3f8585b6d64f2d9c975
joao <joao182cx@gmail.com>
2019-02-18 16:46:25 -0300

    Criação dos arrays que comtém as palavras que geraram as frases.

---
+++ src/Sentencas.java
@@ -0,0 +1,13 @@
+
+public class Sentencas {
+
+	public static void main(String[] args) {
+		
+		final String[] artigo = {"the", "a", "one", "some", "any"};
+		final String[] substantivo = {"boy", "girl", "dog", "town", "car"};
+		final String[] verbo = {"drove", "jumped", "ran", "walked", "skipped"};
+		final String[] preposicao = {"to", "from", "over", "under", "on"};
+		
+	}
+
+}
